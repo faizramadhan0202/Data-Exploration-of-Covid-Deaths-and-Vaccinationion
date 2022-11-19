eb508897a17d9a837c104f17c8ff5c4298bf7638
faizramadhan0202 <faizramadhan0202@gmail.com>
2022-11-19 11:58:58 +0700

    Update Cleaning Data

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -8,3 +8,19 @@ from PortpolioProject01..NashvilleHousing;
 -- Standardlize Date Format (YYYY-MM-DD)
 select SaleDate, (CONVERT(date,SaleDate)) as NewDate
 from PortpolioProject01..NashvilleHousing;
+
+-- Update SaleDate
+Update PortpolioProject01..NashvilleHousing
+Set SaleDate = CONVERT(date, SaleDate);
+
+-- Alter Table Add Column SaleDateConverted
+Alter Table PortpolioProject01..NashvilleHousing
+Add SaleDateConverted Date;
+
+-- Update SaleDateConverted
+Update PortpolioProject01..NashvilleHousing
+Set SaleDateConverted = CONVERT(Date, SaleDate);
+
+-- Select SaleDateConverted
+select SaleDateConverted, (CONVERT(date,SaleDate))
+from PortpolioProject01..NashvilleHousing;
