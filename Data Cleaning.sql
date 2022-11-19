f9545bf60260da7cad4cd2dfd5d83462514c8b26
faizramadhan0202 <faizramadhan0202@gmail.com>
2022-11-19 16:44:34 +0700

    Update Cleaning Data

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -24,3 +24,32 @@ Set SaleDateConverted = CONVERT(Date, SaleDate);
 -- Select SaleDateConverted
 select SaleDateConverted, (CONVERT(date,SaleDate))
 from PortpolioProject01..NashvilleHousing;
+
+-- Populate Property Address Data
+select PropertyAddress
+from PortpolioProject01..NashvilleHousing
+where PropertyAddress is null
+
+-- Select ParcelID
+select *
+from PortpolioProject01..NashvilleHousing
+order by ParcelID
+
+-- Join ParcelID and PropertyAddress
+select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+from PortpolioProject01..NashvilleHousing a
+Join PortpolioProject01..NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is Null
+
+-- Update Null PropertyAddress
+Update a
+Set PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+from PortpolioProject01..NashvilleHousing a
+	Join PortpolioProject01..NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is Null
+
+-- Breaking out Address into Individual Columns (Address, City, State)
